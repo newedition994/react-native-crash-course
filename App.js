24353900291f14912713013cb2f215f964861716
Breyanna Vass <breyannavass@gmail.com>
2020-02-12 13:46:47 -0500

    made the add item file

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 import Header from './components/Header';
 import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 import {uuid} from 'uuidv4';
 
