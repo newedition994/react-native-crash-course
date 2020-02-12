6abf732ce659694a6ef132e0804170c112db92c0
Breyanna Vass <breyannavass@gmail.com>
2020-02-12 13:37:17 -0500

    made it possible to delete an item from the list

--- App.js
+++ App.js
@@ -14,12 +14,20 @@ const App = () => {
     {id: uuid(), text: 'Juice'},
   ]);
 
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Header title="Shopping List" />
       <FlatList
         data={items}
-        renderItem={({item}) => <ListItem item={item} />}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
       />
     </View>
   );
--- components/ListItem.js
+++ components/ListItem.js
@@ -3,12 +3,17 @@ import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
-const ListItem = ({item}) => {
+const ListItem = ({item, deleteItem}) => {
   return (
     <TouchableOpacity style={styles.listItem}>
       <View style={styles.listItemView}>
         <Text style={styles.listItemText}>{item.text}</Text>
-        <Icon name="remove" size={20} color="firebrick" />
+        <Icon
+          name="remove"
+          size={20}
+          color="firebrick"
+          onPress={() => deleteItem(item.id)}
+        />
       </View>
     </TouchableOpacity>
   );
