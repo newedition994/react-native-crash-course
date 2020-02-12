10d568029acb7b138ae30353256946fc431349e4
Breyanna Vass <breyannavass@gmail.com>
2020-02-12 13:08:47 -0500

    making a header

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-      <Image
-        source={{uri: 'https://randomuser.me/api/portraits/women/2.jpg'}}
-        style={styles.img}
-      />
+      <Text>Hello World</Text>
     </View>
   );
 };
@@ -16,16 +12,6 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: 'darkslateblue',
-    fontSize: 30,
-  },
-  img: {
-    width: 100,
-    height: 100,
   },
 });
 
