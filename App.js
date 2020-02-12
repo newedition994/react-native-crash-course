5b8725ea363216f676659cd413d3857d2dc9fc88
Breyanna Vass <breyannavass@gmail.com>
2020-02-12 13:31:43 -0500

    made the list styles and added an icon

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
+
 import Header from './components/Header';
+import ListItem from './components/ListItem';
 
 import {uuid} from 'uuidv4';
 
@@ -17,7 +19,7 @@ const App = () => {
       <Header title="Shopping List" />
       <FlatList
         data={items}
-        renderItem={({item}) => <Text>{item.text}</Text>}
+        renderItem={({item}) => <ListItem item={item} />}
       />
     </View>
   );
