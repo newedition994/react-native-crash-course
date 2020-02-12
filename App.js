e7b4654d3dfb4425671f0a9e0990b3bb1467773e
Breyanna Vass <breyannavass@gmail.com>
2020-02-12 12:57:44 -0500

    using stylesheets for formatting

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'darkslateblue',
+    fontSize: 30,
+  },
+});
 
 export default App;
