46bbb0c0bf6c9ed11cd536027f7d2ba210abfe93
Breyanna Vass <breyannavass@gmail.com>
2020-02-12 13:21:09 -0500

    created items that are displayed for the list

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
 
+import {uuid} from 'uuidv4';
+
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Bread'},
+    {id: uuid(), text: 'Juice'},
+  ]);
+
   return (
     <View style={styles.container}>
-      <Header />
+      <Header title="Shopping List" />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   );
 };
--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-const Header = () => {
+const Header = ({title}) => {
   return (
     <View style={styles.header}>
-      <Text style={styles.text}>Shopping List</Text>
+      <Text style={styles.text}>{title}</Text>
     </View>
   );
 };
